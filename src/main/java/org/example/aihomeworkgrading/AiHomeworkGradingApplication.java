ea446d23d89c4d3b3ccb55aa8509014398742a60
staycalm <hello@staycalm.cn>
2026-04-29 16:46:09 +0800

    项目的初始化

--- src/main/java/org/example/aihomeworkgrading/AiHomeworkGradingApplication.java
+++ src/main/java/org/example/aihomeworkgrading/AiHomeworkGradingApplication.java
@@ -2,7 +2,7 @@ package org.example.aihomeworkgrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//启动类
 @SpringBootApplication
 public class AiHomeworkGradingApplication {
 
