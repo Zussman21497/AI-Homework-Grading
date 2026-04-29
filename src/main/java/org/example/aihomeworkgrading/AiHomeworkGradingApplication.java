4c1c6816a3b43111f9f51d424dc994421fb0cedf
staycalm <hello@staycalm.cn>
2026-04-29 16:43:42 +0800

    项目的初始化

---
+++ src/main/java/org/example/aihomeworkgrading/AiHomeworkGradingApplication.java
@@ -0,0 +1,13 @@
+package org.example.aihomeworkgrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHomeworkGradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiHomeworkGradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aihomeworkgrading/AiHomeworkGradingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aihomeworkgrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHomeworkGradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
